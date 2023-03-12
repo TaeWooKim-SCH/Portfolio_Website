f37b5e37900d7a66c857b817bda4012220eb60fa
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2023-03-12 18:14:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+
+// navbar button click move
+const btn_home = document.querySelector('#btn_home');
+// btn_home.addEventListener('click', () => {
+//     window.scrollTo(0, 0);
+//     // btn_home.style.transition = "red";
+// })
+
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if (link === undefined) {
+        return;
+    }
+    console.log(target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+})
